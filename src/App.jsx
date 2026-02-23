c7aa97cad778b820b5ef30b04091436e71455f97
Hariom Singh <suppraaaa2003@gmail.com>
2026-02-23 15:10:15 +0530

    some error due to route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Nevbar from './component/Nevbar'
+import Hero from './component/Hero'
+import About from './component/About'
+import Contact from './component/Contact'
 
 
 const App = () => {
   return (
-    <Nevbar/>
+    {/* Nev bar is there is above rute but data will lode in rutes */}
+      <Nevbar/>
+      {/* Hair u have to give specific rute to each page thats imp */}
+      <Routes>
+        <Route path='/' element={ <Hero/> } />
+        <Route path='/about' element={<About/>  } />
+        <Route path='/contact' element={ <Contact/> } />
+        {/* <Route path='/product' element={<Product/>} /> */}
+        
+      </Routes>
   )
 }
 
--- src/component/Nevbar.jsx
+++ src/component/Nevbar.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Nevbar = () => {
   return (
-    <div>Nevbar</div>
+    <div>
+       <div className='nev'>
+        <h2>Thala</h2>
+        <div>
+            
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
+            <Link to='/product'>Product</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
